65989b803d094fb8a682f47a7bf4fa6c6aacfd44
Giano-hub <giovatesi98@gmail.com>
2026-07-23 14:52:49 +0100

    Update game_controls.js

--- docs/js/game_controls.js
+++ docs/js/game_controls.js
@@ -38,12 +38,6 @@ $(".game").change(function() {
 	$(".game-specific").not(".gm" + gameId).hide();
 	setSwitchGuide();
 	
-	if (game !== "None" && typeof restoreLastTrainer === "function") {
-	    if (restoreLastTrainer()) {
-		     $(".opposing").change();
-	    }
-   }
-	
 	if (gameId > 0 && GAME_FEATURES[game].sync) $("#sync.bs-btn").show();
 	else $("#sync.bs-btn").hide();
 
